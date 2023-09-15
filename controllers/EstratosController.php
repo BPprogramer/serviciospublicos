e74662d5684caef0d2cd7749636dd74cfffcb7c2
BPprogramer <piambabryan20@gmail.com>
2023-09-14 19:16:26 -0500

    ultimos_ajustes

--- controllers/EstratosController.php
+++ controllers/EstratosController.php
@@ -19,7 +19,7 @@
 
             if($_SERVER['REQUEST_METHOD']=='POST'){
 
-                debuguear($_POST);
+            
                 
              
                 $estratos->sincronizar($_POST);
